693a0c40709ee916060026b0707f715a53af8375
Asad Chaudhary <asad.chaudhary@vmdomain.com>
2025-06-12 02:52:19 +0530

    Only letters Updated

--- string/OnlyLetters.js
+++ string/OnlyLetters.js
@@ -1,17 +1,36 @@
-let input = "abc1";
-let number = [1,2,3,4,5,6,7,8,9,0];
+let input = "abc12";
 
-
-function letterChecker(str){
+function lChecker(str){
     for(let i=0;i<str.length;i++){
-        if(input[i]== number[i]){
+        if(str[i]>=0 && str[i]<9){
             console.log("false");
-        }
-        else{
-            console.log("true");
+            return;
         }
     }
-    
+    console.log("true");
 }
 
+// -------------------------------------------------------------------
+
+let numbers = [1,2,3,4,5,6,7,8,9,0];
+function letterChecker(str){
+    temp = "";
+    counter = 0;
+    for(let i=0;i<str.length;i++){
+        temp = str[i];
+        for(let j=0;j<numbers.length-1;j++){
+            if(temp == numbers[j]){
+                counter++;
+            }
+        }
+    }
+    if(counter>0){
+        console.log("false");
+    }    
+    else{
+        console.log("true");
+    }
+}
+
+lChecker(input);
 letterChecker(input);
