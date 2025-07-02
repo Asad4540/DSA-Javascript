2858ac843adcc987df48e834aee94c402d6607bf
Asad Chaudhary <asad.chaudhary@vmdomain.com>
2025-07-02 23:16:24 +0530

    PlusOne Optimized

--- Leetcode/66. PlusOne.js
+++ Leetcode/66. PlusOne.js
@@ -1,17 +1,33 @@
-digits = [9,5];
-
+digits = [9, 9];
 
 var plusOne = function (digits) {
-    result = digits.join('');
-    let final = [];
-    if (result >= 0) {
-        result++;
-        let str = result.toString();
-        for (let i = 0; i < str.length; i++) {
-            final.push(Number(str.charAt(i)));
+    for (let i = digits.length - 1; i >= 0; i--) {
+        if (digits[i] < 9) {
+            digits[i] += 1;
+            return digits;
+        }
+        digits[i] = 0;
+        if (i == 0) {
+            digits.unshift(1);
+            return digits;
         }
     }
-    return final;
+};
 
-}
 console.log(plusOne(digits));
+
+
+
+// var plusOne = function (digits) {
+//     result = digits.join('');
+//     let final = [];
+//     if (result >= 0) {
+//         result++;
+//         let str = result.toString();
+//         for (let i = 0; i < str.length; i++) {
+//             final.push(Number(str.charAt(i)));
+//         }
+//     }
+//     return final;
+
+// }
