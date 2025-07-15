e5578b8930215b068833cf2b2090c8603f9ff42a
Asad Chaudhary <asad.chaudhary@vmdomain.com>
2025-07-15 19:39:17 +0530

    Valid Word

---
+++ Leetcode/3136. ValidWord.js
@@ -0,0 +1,18 @@
+input = "Ya$";
+
+var isValid = function (word) {
+    let vowels = "aeiouAEIOU", hasVowel = false; hasConsonant = false;
+    let consonants = "bcdfghjklmnpqrstvqxyzBCDFGHJKLMNPQRSTVWXYZ"
+    if (word.length < 3) return false;
+    for (let i = 0; i < word.length; i++) {
+        let ch = word[i];
+        if (!/[a-zA-Z0-9]/.test(ch)) return false;
+        if (vowels.includes(ch)) hasVowel = true;
+        else if (consonants.includes(ch)) hasConsonant = true;
+    }
+    if (hasVowel && hasConsonant) return true;
+    return false;
+};
+
+
+console.log(isValid(input));
