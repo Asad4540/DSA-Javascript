fc837732a3778e98c183bcf2b44a728d0678cb85
Asad Chaudhary <asad.chaudhary@vmdomain.com>
2025-07-19 02:58:52 +0530

    updated Missing Number

--- string/MissingNumber.js
+++ string/MissingNumber.js
@@ -2,7 +2,12 @@ input = [2, 5, 6, 7, 4, 1]
 
 function MissingNumber(num) {
     temp = num.sort();
-    for (let i = 0; i < temp.length; i++){
-        
+    for (let i = temp[0]; i < temp.length; i++) {
+        if (!num.includes(i)) {
+            // console.log(i);
+            return i;
+        }
     }
 }
+
+console.log(MissingNumber(input));
